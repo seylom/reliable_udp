e35abe67a66397b88d97c75b9821e325d69bdbdc
faran <faran@localhost.localdomain>
2014-05-05 13:40:51 -0500

    simplification

--- MP3-receiver.c
+++ MP3-receiver.c
@@ -15,7 +15,7 @@
 #include "helper.h"
 
 #define MAXBUFLEN 1500
-#define WINDOW_SIZE 200
+#define WINDOW_SIZE 100
 
 typedef struct packet_info{
     char* hostname;
@@ -23,26 +23,32 @@ typedef struct packet_info{
 };
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile);
-void sigchld_handler(int s);
+//void sigchld_handler(int s);
 int establish_receive_connection();
-int establisb_send_connection(char* hostname);
-void send_dgram(char *host, char *port, reliable_dgram *dgram);
-void process_packet(struct packet_info *packet);
+int establish_send_connection(char* hostname);
+//void send_dgram(char *host, char *port, reliable_dgram *dgram);
+//void process_packet(struct packet_info *packet);
 void write_to_file();
 void initialize_window();
-void *packet_handler(void *datapv);
+//void *packet_handler(void *datapv);
 int map_seq_to_window(int seq);
 void *write_handler(void *datapv);
 
 struct sockaddr_storage their_addr;
 char destination[256];
 
-typedef struct window_slot{
+char* sender_host_name = NULL;
+
+int done = 0 ;
+
+int send_sock;
+
+struct window_slot{
      int ack;
      int written;
      int received;
      int seq;
-     
+     int size;
      char *data;
 	 char *hostname;
 };
@@ -72,7 +78,6 @@ int map_seq_to_window(int seq) {
 }
 
 int window_has_room() {
-
 	if (available_slots > 0)
 		return 1;
 	return 0;
@@ -106,6 +111,7 @@ void initialize_window(){
         window[i].written = 0;
         window[i].received = 0;   
         window[i].seq = 0;
+        window[i].size = 0 ;
     }
     
     available_slots = WINDOW_SIZE;
@@ -121,30 +127,30 @@ void *get_in_addr(struct sockaddr *sa) {
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
 
-    if (access(destinationFile, F_OK) != -1){
-        if (remove(destinationFile) == 0){
-            //printf("reliable_receiver: existing destination file removed\n");
-        }
-    }
+//    if (access(destinationFile, F_OK) != -1){
+//        if (remove(destinationFile) == 0){
+//            //printf("reliable_receiver: existing destination file removed\n");
+//        }
+//    }
     
-    file = fopen(destinationFile, "a");
+    file = fopen(destinationFile, "w");
     
     if (file == NULL){
         printf("reliable_receiver: Unable to create the destination file\n");
         exit(1);
     }
     
-    fclose(file);
-    
-    memcpy(destination, destinationFile, strlen(destinationFile));
-    destination[strlen(destinationFile)] = 0;
+//    fclose(file);
+//
+//    memcpy(destination, destinationFile, strlen(destinationFile));
+//    destination[strlen(destinationFile)] = 0;
     
 	int sockfd = establish_receive_connection();
 	
 	pthread_t thread;
 	pthread_create(&thread, NULL, (void*)write_handler,(void*)NULL);
 	
-	while (1) {
+	while (!done) {
 		receivePacket(sockfd);
 	}
 }
@@ -162,7 +168,7 @@ void *write_handler(void *datapv){
 
 void receivePacket(int sockfd) {
 	int numbytes;
-	char buf[MAXBUFLEN];
+	unsigned char buf[MAXBUFLEN];
 	char s[INET6_ADDRSTRLEN];
 	socklen_t addr_len = sizeof their_addr;
 
@@ -171,106 +177,155 @@ void receivePacket(int sockfd) {
 		perror("recvfrom");
 		exit(1);
 	}
-	
-	char* sender_host = inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *) &their_addr), s, sizeof s);
-	
-	reliable_dgram *dgram = (reliable_dgram *)buf;
-	
-	struct packet_info *packet;
-	packet = malloc(sizeof(struct packet_info));
-	
-	packet->hostname = malloc(256);
-	memcpy(packet->hostname, sender_host, strlen(sender_host));
-	packet->hostname[strlen(sender_host)] = 0;
-	
-	packet->datagram = malloc(sizeof(*dgram));
-	*(packet->datagram) = *dgram;
-	
-	//pthread_t thread;
-	//pthread_create(&thread, NULL, (void*)packet_handler, (void*)packet);
-	
-	//printf("Thread id: %u\n", thread);
-	
-	packet_handler((void*)packet);
-	
+
+	// 4 bytes is end of stream notification
+	if(numbytes == 4) {
+		sleep(2);
+		done = 1 ;
+	} else {
+		if (sender_host_name == NULL) {
+			sender_host_name = inet_ntop(their_addr.ss_family,
+				get_in_addr((struct sockaddr *) &their_addr), s, sizeof s);
+			send_sock = establish_send_connection(sender_host_name);
+		}
+
+		int seq;
+		int size;
+		memcpy(&seq, buf, sizeof(int));
+		memcpy(&size, buf + sizeof(int), sizeof(int));
+		unsigned char* payload = malloc(size);
+		memcpy(payload, buf + 2*sizeof(int), size);
+
+		if (available_slots > 0){
+			if (seq > (window_start + WINDOW_SIZE)){
+				printf("Packet with seq #%d out of bound for window\n",seq);
+				return;
+			}
+
+			available_slots--;
+			//store packet in window
+			int slot = map_seq_to_window(seq);
+			if (window[slot].received == 1 && window[slot].written == 0){
+				printf("reliable_receiver: Attempt to override packet not yet consumed\n");
+				return;
+			}
+			window[slot].received = 1;
+			window[slot].written = 0;
+			window[slot].ack = 0;
+			window[slot].seq = seq;
+			window[slot].size = size;
+			window[slot].data = payload;
+		}
+	}
 }
 
-void *packet_handler(void *datapv){
+		//payload[size] = '\0';
+		//printf("packet #%d with size %d has payload %s\n", seq, size, payload);
+
+//		reliable_dgram *dgram = (reliable_dgram *)buf;
+//
+//		struct packet_info *packet;
+//		packet = malloc(sizeof(struct packet_info));
+//
+//		packet->hostname = malloc(256);
+//		memcpy(packet->hostname, sender_host, strlen(sender_host));
+//		packet->hostname[strlen(sender_host)] = 0;
+//
+//		packet->datagram = malloc(sizeof(*dgram));
+//		*(packet->datagram) = *dgram;
+
+		//pthread_t thread;
+		//pthread_create(&thread, NULL, (void*)packet_handler, (void*)packet);
+
+		//printf("Thread id: %u\n", thread);
+
+		//packet_handler((void*)packet);
 	
-    struct packet_info *packet =  (struct packet_info *)datapv;
- 
-    reliable_dgram *dgram = packet->datagram;
-    
-    pthread_mutex_lock(&window_lock);
-     
-    if (available_slots > 0){ 
-        
-		if (dgram->seq > (window_start + WINDOW_SIZE)){
-			printf("Packet with seq #%d out of bound for window\n",dgram->seq);
-			return;
-		}
-	
-        available_slots--;
-        
-	    dgram->payload[DATA_SIZE] = 0;
-	
-	    process_packet(packet);
-	
-	    //write_to_file();
-	}
-	else{
-		printf("Dropping packet seq #%d\n", dgram->seq);
-	}
-	
-	pthread_mutex_unlock(&window_lock);
-}
+
+//void *packet_handler(void *datapv){
+//
+//    struct packet_info *packet =  (struct packet_info *)datapv;
+//
+//    reliable_dgram *dgram = packet->datagram;
+//
+//    pthread_mutex_lock(&window_lock);
+//
+//    if (available_slots > 0){
+//
+//		if (dgram->seq > (window_start + WINDOW_SIZE)){
+//			printf("Packet with seq #%d out of bound for window\n",dgram->seq);
+//			return 1;
+//		}
+//
+//        available_slots--;
+//
+//	    dgram->payload[DATA_SIZE] = 0;
+//
+//	    process_packet(packet);
+//
+//	    //write_to_file();
+//	}
+//	else{
+//		printf("Dropping packet seq #%d\n", dgram->seq);
+//	}
+//
+//	pthread_mutex_unlock(&window_lock);
+//}
 
 void sendAck(char* hostName, int seq, int slots) {
-
-    struct reliable_dgram dgram;
-    
-    dgram.seq = seq;
-    dgram.size = 3*sizeof(char);
-	dgram.window_size = slots;
-	
-    memcpy(dgram.payload,"ACK",dgram.size);
+	unsigned char* ack = malloc(sizeof(int));
+	memcpy(ack, &seq, sizeof(int));
+	int sentBytes;
+	if (client_info) {
+		if ((sentBytes = sendto(send_sock, ack, sizeof(int), 0,
+				client_info->ai_addr, client_info->ai_addrlen)) == -1) {
+			perror("packet send:");
+			exit(1);
+		}
+	}
+}
+//    struct reliable_dgram dgram;
+//
+//    dgram.seq = seq;
+//    dgram.size = 3*sizeof(char);
+//	dgram.window_size = slots;
+//
+//    memcpy(dgram.payload,"ACK",dgram.size);
     
     //printf("reliable_receiver: Sending ACK for seq #%d\n", seq);
-    send_dgram(hostName, send_port, &dgram);
-}
+    //send_dgram(hostName, send_port, seq);
 
 /*
 *   store packet int the window
 */
-void process_packet(struct packet_info *packet){
-
-    reliable_dgram *dgram = packet->datagram;
-	
-    //store packet in window
-    int slot = map_seq_to_window(dgram->seq);
-    
-    if (window[slot].received == 1 && window[slot].written == 0){
-		printf("reliable_receiver: Attempt to override packet not yet consumed\n");
-		return;
-	}
-
-	window[slot].received = 1;
-	window[slot].written = 0;
-	window[slot].ack = 0;
-	window[slot].seq = dgram->seq;
-	
-	window[slot].data = malloc(sizeof(char)*strlen(dgram->payload));
-	memcpy(window[slot].data, dgram->payload, strlen(dgram->payload));
-	(window[slot].data)[strlen(dgram->payload)] = 0;
-	
-	window[slot].hostname = malloc(sizeof(char)*strlen(packet->hostname));
-	memcpy(window[slot].hostname, packet->hostname, strlen(packet->hostname));
-	(window[slot].hostname)[strlen(packet->hostname)] = 0;
-
-	//window_start++;
-	//available_slots--;
-}
+//void process_packet(struct packet_info *packet){
+//
+//    reliable_dgram *dgram = packet->datagram;
+//
+//    //store packet in window
+//    int slot = map_seq_to_window(dgram->seq);
+//
+//    if (window[slot].received == 1 && window[slot].written == 0){
+//		printf("reliable_receiver: Attempt to override packet not yet consumed\n");
+//		return;
+//	}
+//
+//	window[slot].received = 1;
+//	window[slot].written = 0;
+//	window[slot].ack = 0;
+//	window[slot].seq = dgram->seq;
+//
+//	window[slot].data = malloc(sizeof(char)*strlen(dgram->payload));
+//	memcpy(window[slot].data, dgram->payload, strlen(dgram->payload));
+//	(window[slot].data)[strlen(dgram->payload)] = 0;
+//
+//	window[slot].hostname = malloc(sizeof(char)*strlen(packet->hostname));
+//	memcpy(window[slot].hostname, packet->hostname, strlen(packet->hostname));
+//	(window[slot].hostname)[strlen(packet->hostname)] = 0;
+//
+//	//window_start++;
+//	//available_slots--;
+//}
 
 /*
 *Writes the provided data to the destination file
@@ -283,15 +338,9 @@ void write_to_file(){
     int written_count = 0;
     
     //only write if we have packets in the correct order
-	
 	for(i = window_start; i < window_start + WINDOW_SIZE; i++){
     
-	//while(window[map_seq_to_window(window_start)].received == 1 && 
-	//	  window[map_seq_to_window(window_start)].ack == 0 &&
-	//	  window[map_seq_to_window(window_start)].written == 0){
-    
         int idx =  map_seq_to_window(i);
-		
 		if (window[idx].received == 1 && window[idx].written == 1){
 			window_start++;
 		}
@@ -301,21 +350,21 @@ void write_to_file(){
             
 		printf("reliable_receiver: writting seq#%d of size %d to file from slot %d\n", window[idx].seq, strlen(window[idx].data), idx);
 		
-		file = fopen(destination,"a");  
-		fputs(window[idx].data, file);
-		fclose(file);
+		//file = fopen(destination,"a");
+		fwrite(window[idx].data, 1, window[idx].size , file);
+		//fclose(file);
 		
 		available_slots++;
 		
 		//send the ack
-		sendAck(window[idx].hostname, window[idx].seq, available_slots);
+		sendAck(sender_host_name, window[idx].seq, available_slots);
 		
 		window[idx].received = 0;
 		free(window[idx].data);
 		window[idx].data = NULL;
 
-		free(window[idx].hostname);
-		window[idx].hostname = NULL;
+		//free(window[idx].hostname);
+		//window[idx].hostname = NULL;
 		
 		window[idx].seq = 0;
 		window[idx].ack = 0;
@@ -417,51 +466,50 @@ int establish_send_connection(char* hostname) {
 /*
 *   sends UDP message
 */
-void send_dgram(char *host, char *port, reliable_dgram *dgram){
-
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    int numbytes;
-
-    //char *message = (char*)dgram;
-    
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    if ((rv = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-
-    // loop through all the results and make a socket
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("node: socket");
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "node: failed to bind socket\n");
-        return 2;
-    }
-
-    if ((numbytes = sendto(sockfd, (char*)dgram, MAXBUFLEN - 1, 0,
-             p->ai_addr, p->ai_addrlen)) == -1) {
-        perror("node: sendto");
-        exit(1);
-    }
-
-    freeaddrinfo(servinfo); 
-    
-    close(sockfd);
-}
+//void send_dgram(char *host, char *port, int seq){
+//    int sockfd;
+//    struct addrinfo hints, *servinfo, *p;
+//    int rv;
+//    int numbytes;
+//
+//    //char *message = (char*)dgram;
+//
+//    memset(&hints, 0, sizeof hints);
+//    hints.ai_family = AF_INET;
+//    hints.ai_socktype = SOCK_DGRAM;
+//    hints.ai_flags = AI_PASSIVE;
+//
+//    if ((rv = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
+//        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+//        return ;
+//    }
+//
+//    // loop through all the results and make a socket
+//    for(p = servinfo; p != NULL; p = p->ai_next) {
+//        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+//                p->ai_protocol)) == -1) {
+//            perror("node: socket");
+//            continue;
+//        }
+//
+//        break;
+//    }
+//
+//    if (p == NULL) {
+//        fprintf(stderr, "node: failed to bind socket\n");
+//        return ;
+//    }
+//
+//    if ((numbytes = sendto(sockfd, (char*)dgram, MAXBUFLEN - 1, 0,
+//             p->ai_addr, p->ai_addrlen)) == -1) {
+//        perror("node: sendto");
+//        exit(1);
+//    }
+//
+//    freeaddrinfo(servinfo);
+//
+//    close(sockfd);
+//}
 
 void sigchld_handler(int s) {
 	while (waitpid(-1, NULL, WNOHANG) > 0)
--- MP3-sender.c
+++ MP3-sender.c
@@ -24,12 +24,13 @@
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport,
 		char* filename, unsigned long long int bytesToTransfer);
-void sendPacket(char* packet);
-int establisb_send_connection(char* host);
+void sendPacket(unsigned char* packet);
+int establish_send_connection(char* host);
 int establish_receive_connection();
 int is_window_entry_timedout(int index);
 void *listen_for_ack(void* data);
 int window_has_room();
+void send_eof_notification();
 
 struct addrinfo hints, *servinfo, *p;
 struct addrinfo *sender_info, *receiver_info;
@@ -80,7 +81,7 @@ void init(char* filename, int udpPort) {
 
 	// Convert port to string
 	sprintf(port, "%d", udpPort);
-	sprintf(ack_port,"%d", udpPort + 5);
+	sprintf(ack_port, "%d", udpPort + 5);
 }
 
 int main(int argc, char** argv) {
@@ -106,104 +107,97 @@ void reliablyTransfer(char* hostName, unsigned short int udpPort,
 	init(fileName, udpPort);
 
 	/* Opens the connection for sending packets */
-	send_socket = establisb_send_connection(hostName);
+	send_socket = establish_send_connection(hostName);
 
 	/* Start listening for ack */
 	pthread_t thread;
 	pthread_create(&thread, NULL, (void*) listen_for_ack, NULL);
 	//sleep(1);
 
-    int notified_completed = 0;
-    int expected_ack = 0;
-    int read_bytes = 0;
+	int expected_ack = 0;
+	int read_bytes = 0;
 	int total_ack_bytes = 0;
-    
-    printf("Max number of bytes to send: %d\n", numBytes);
-    
+
+	printf("Max number of bytes to send: %d\n", numBytes);
+
 	/* Loop through the file content and send packets to fill a window */
 	while (1) {
-		// Sending next packet if there is room in sliding window
+		// Check if file is all read or enough bytes are sent
 		if (feof(fp) || read_bytes >= numBytes) {
-			if (notified_completed == 0){
-			    notified_completed = 1;
-			    printf("reliable_sender: All file blocks were fully added to the window.\n");
+			printf(
+					"reliable_sender: All file blocks were fully added to the window.\n");
+			send_eof_notification();
+			break;
+		} else if (window_has_room()) { // Sending next packet if there is room in sliding window
+			pthread_mutex_lock(&lock);
+			// Calculating how many bytes to pack into the packet
+			int actual_data_size = 0;
+			if (numBytes - read_bytes < DATA_SIZE) {
+				actual_data_size = numBytes - read_bytes;
+			} else {
+				actual_data_size = DATA_SIZE;
 			}
-			
+
+			unsigned char* packet = malloc(PACKET_SIZE);
+			unsigned char* data_block = malloc(actual_data_size);
+			size_t content_size = fread(data_block, 1, actual_data_size, fp);
+
+			read_bytes += content_size;
+
+			/* Copy sequence number  to packet */
+			memcpy(packet, &current_seq, INT_SIZE);
+
+			/* Copy payload size to packet */
+			memcpy(packet + INT_SIZE, &content_size, INT_SIZE);
+
+			/* Copy payload to packet */
+			memcpy(packet + HEADER_SIZE, data_block, content_size);
+
+			/* Create a window entry */
+			int index = map_seq_to_window(current_seq);
+			struct SlidingWindow entry = window[index];
+			entry.seq = current_seq;
+			entry.data = packet;
+			entry.ack = 0;
+			entry.time_sent = (int) time(NULL);
+			entry.size = content_size;
+
+			printf(
+					"reliable_sender: sending seq #%d - payload %d bytes| %d/%d bytes\n",
+					current_seq, strlen(data_block), read_bytes, numBytes);
+
+			sendPacket(packet);
+			current_seq++;
+
+			pthread_mutex_unlock(&lock);
+		} else {
+			printf("Window is full\n");
 			//sleep(2);
 			usleep(100000);
-			//TODO: check if all packets are acked successfully then exit.
-		} else{
-		    if (window_has_room()) {
-			    pthread_mutex_lock(&lock);
-			    
-			    int actual_data_size = 0;
-			    
-			    if(numBytes - read_bytes < DATA_SIZE){
-			        actual_data_size = numBytes - read_bytes;
-			    }
-			    else{
-			        actual_data_size = DATA_SIZE;
-			    }
-			        
-			    char* packet = calloc(PACKET_SIZE + HEADER_SIZE, 1);
-			    char* data_block = malloc(actual_data_size);
-			    size_t content_size = fread(data_block, 1, actual_data_size, fp);
-				data_block[content_size] = 0;
-
-                read_bytes += content_size;
-                
-			    /* Copy sequence number  to packet */
-			    memcpy(packet, &current_seq, INT_SIZE);
-
-			    /* Copy payload size to packet */
-			    memcpy(packet + INT_SIZE, &content_size, INT_SIZE);
-
-			    /* Copy payload to packet */
-			    memcpy(packet + HEADER_SIZE, data_block, content_size);
-
-			    /* Create a window entry */
-			    int index = map_seq_to_window(current_seq);
-			    window[index].seq = current_seq;
-			    window[index].data = packet;
-			    window[index].ack = 0;
-			    window[index].time_sent = (int) time(NULL);
-			    window[index].size = actual_data_size;
-			    
-			    printf("reliable_sender: sending seq #%d - payload %d bytes| %d/%d bytes\n", current_seq, strlen(data_block),read_bytes, numBytes);
-			    
-			    sendPacket(packet);
-			    current_seq++;
-			    
-			    pthread_mutex_unlock(&lock);
-		    } else {
-			    printf("Window is full\n");
-			    //sleep(2);
-				usleep(100000);
-		    }
 		}
 		// check sent packets and re-send timed out ones
-		
-		int expected_ack = 0;
-		
+
 		int i = 0;
 		for (i = 0; i < WINDOW_SIZE; i++) {
 			struct SlidingWindow entry = window[i];
 			if (entry.data && is_window_entry_timedout(i)) {
-			    
+
 				printf("packet %d is timed out.\n", entry.seq);
 				entry.time_sent = (int) time(NULL);
-				
-				printf("reliable_sender: Re-sending seq #%d of payload size %d\n", entry.seq, entry.size);
-				
+
+				printf(
+						"reliable_sender: Re-sending seq #%d of payload size %d\n",
+						entry.seq, entry.size);
+
 				sendPacket(entry.data);
-				
+
 				expected_ack++;
 			}
 		}
-		
-		if (total_ack_bytes == numBytes){
-		    printf("File successfully transferred!\n");
-		    break;
+
+		if (total_ack_bytes == numBytes) {
+			printf("File successfully transferred!\n");
+			break;
 		}
 	}
 }
@@ -217,48 +211,163 @@ int is_window_entry_timedout(int index) {
 	return 0;
 }
 
+/* Send a notification of 4 bytes to receiver notifying it that transfer is over */
+void send_eof_notification() {
+	char buffer[] = "DONE";
+	int numbytes;
+	if (receiver_info) {
+		if ((numbytes = sendto(send_socket, buffer, strlen(buffer), 0,
+				receiver_info->ai_addr, receiver_info->ai_addrlen)) == -1) {
+
+			perror("packet send:");
+			exit(1);
+		}
+	}
+}
+
 int window_has_room() {
 	if (window_start + WINDOW_SIZE > current_seq)
 		return 1;
 	return 0;
 }
 
-void ack_packet(int seq, int receiver_window) {
+void ack_packet(int seq) {
 	pthread_mutex_lock(&lock);
 
 	int index = map_seq_to_window(seq);
-	
-	if(window[index].ack == 0){
+
+	if (window[index].ack == 0) {
 		struct SlidingWindow entry = window[index];
 		window[index].ack = 1;
 		window[index].seq = 0;
-		
+
 		int data_size = window[index].size;
 		num_bytes_sent += data_size;
-		
+
 		free(window[index].data);
 		window[index].data = NULL;
-	}
-	else{
+	} else {
 		printf("reliable_sender: Duplicate ACK received for seq #%d\n", seq);
 	}
 
-	// Slide window as more packets get ack
+// Slide window as more packets get ack
 	int slide_value = 0;
-	while (window[map_seq_to_window(window_start + 1)].ack && slide_value < WINDOW_SIZE-1 && receiver_window > 0) {
+	while (window[map_seq_to_window(window_start + 1)].ack
+			&& slide_value < WINDOW_SIZE - 1 /*&& receiver_window > 0*/) {
 		slide_value++;
 		window_start++;
 		window[map_seq_to_window(window_start)].ack = 0; // reseting ack
-		printf("reliable_sender: window start for seq# %d is set to %d\n", seq, window_start);
-		
-		receiver_window--;
-		
-		if (receiver_window == 0){
-			printf("reliable_sender: Reached receiver advertized limit %d\n");
+		printf("reliable_sender: window start for seq# %d is set to %d\n", seq,
+				window_start);
+
+//		receiver_window--;
+//
+//		if (receiver_window == 0) {
+//			printf("reliable_sender: Reached receiver advertized limit %d\n");
+//		}
+	}
+
+	pthread_mutex_unlock(&lock);
+}
+
+void sendPacket(unsigned char* packet) {
+	int size;
+	memcpy(&size, packet + sizeof(int), sizeof(int));
+	int sentBytes;
+	if (receiver_info) {
+		if ((sentBytes = sendto(send_socket, packet, size + HEADER_SIZE, 0,
+				receiver_info->ai_addr, receiver_info->ai_addrlen)) == -1) {
+
+			perror("packet send:");
+			exit(1);
 		}
 	}
-    
-	pthread_mutex_unlock(&lock);
+}
+//void sendPacket(char* packet) {
+//	char buffer[PACKET_SIZE + 1];
+//	int seq;
+//	int size;
+//	memcpy(&seq, packet, sizeof(int));
+//	memcpy(&size, packet + sizeof(int), sizeof(int));
+//	memcpy(buffer, packet + HEADER_SIZE, PACKET_SIZE);
+//	buffer[PACKET_SIZE] = '\0';
+//
+//	struct reliable_dgram dgram;
+//
+//	dgram.seq = seq;
+//	dgram.size = size;
+//	dgram.next_seq = seq + 1;
+//	memcpy(dgram.payload, buffer, PACKET_SIZE);
+//
+//	int numbytes;
+//
+//    if (receiver_info){
+//	    if ((numbytes = sendto(send_socket, (char*)(&dgram), PACKET_SIZE + HEADER_SIZE, 0,
+//	            receiver_info->ai_addr,
+//	                receiver_info->ai_addrlen )) == -1) {
+//
+//		    perror("packet send:");
+//		    exit(1);
+//	    }
+//	}
+//}
+
+void *listen_for_ack(void* data) {
+	printf("listening thread is started ...\n");
+	receive_socket = establish_receive_connection();
+	struct sockaddr_storage their_addr;
+	socklen_t addr_len = sizeof their_addr;
+	while (1) {
+		int numbytes;
+		char buf[PACKET_SIZE];
+		if ((numbytes = recvfrom(receive_socket, buf, PACKET_SIZE - 1, 0,
+				(struct sockaddr *) &their_addr, &addr_len)) == -1) {
+			perror("ack recv");
+			exit(1);
+		}
+
+		//int ack_seq;
+
+		reliable_dgram *dgram = (reliable_dgram *) buf;
+
+		//memcpy(&ack_seq, buf, INT_SIZE);
+		//ack_packet(ack_seq);
+		ack_packet(dgram->seq);
+	}
+}
+
+int establish_send_connection(char* hostname) {
+	int sockfd;
+	int rv;
+	struct addrinfo hints, *servinfo, *p;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+// loop through all the results and make a socket
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			perror("talker: socket");
+			continue;
+		}
+
+		break;
+	}
+	if (p == NULL) {
+		fprintf(stderr, "talker: failed to bind socket\n");
+		return 2;
+	}
+	receiver_info = malloc(sizeof *p);
+	*receiver_info = *p;
+	freeaddrinfo(servinfo);
+	return sockfd;
 }
 
 int establish_receive_connection() {
@@ -276,7 +385,7 @@ int establish_receive_connection() {
 		return 1;
 	}
 
-	// loop through all the results and bind to the first we can
+// loop through all the results and bind to the first we can
 	for (p = servinfo; p != NULL; p = p->ai_next) {
 		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
 				== -1) {
@@ -296,105 +405,10 @@ int establish_receive_connection() {
 	if (p == NULL) {
 		fprintf(stderr, "listener: failed to bind socket\n");
 		return 2;
-	}	
-	
+	}
 	sender_info = malloc(sizeof *p);
-	
 	*sender_info = *p;
-
 	freeaddrinfo(servinfo);
-	//printf("listener: waiting to recvfrom...\n");
+//printf("listener: waiting to recvfrom...\n");
 	return sockfd;
-}
-
-int establisb_send_connection(char* hostname) {
-	int sockfd;
-	int rv;
-	struct addrinfo hints, *servinfo, *p;
-	memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE;
-
-	if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and make a socket
-	for (p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
-				== -1) {
-			perror("talker: socket");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "talker: failed to bind socket\n");
-		return 2;
-	}
-	
-	receiver_info = malloc(sizeof *p);
-	
-	*receiver_info = *p;
-	
-	freeaddrinfo(servinfo);
-	
-	return sockfd;
-}
-
-void sendPacket(char* packet) {
-	char buffer[PACKET_SIZE + 1];
-	int seq;
-	int size;
-	memcpy(&seq, packet, sizeof(int));
-	memcpy(&size, packet + sizeof(int), sizeof(int));
-	memcpy(buffer, packet + HEADER_SIZE, PACKET_SIZE);
-	buffer[PACKET_SIZE] = '\0';
-	
-	struct reliable_dgram dgram;
-	
-	dgram.seq = seq;
-	dgram.size = size;
-	dgram.next_seq = seq + 1;
-	memcpy(dgram.payload, buffer, PACKET_SIZE);
-	
-	int numbytes;
-
-    if (receiver_info){
-	    if ((numbytes = sendto(send_socket, (char*)(&dgram), PACKET_SIZE + HEADER_SIZE, 0, 
-	            receiver_info->ai_addr, 
-	                receiver_info->ai_addrlen )) == -1) {
-	                
-		    perror("packet send:");
-		    exit(1);
-	    }
-	}
-}
-
-void *listen_for_ack(void* data) {
-	printf("listening thread is started ...\n");
-	receive_socket = establish_receive_connection();
-	struct sockaddr_storage their_addr;
-	socklen_t addr_len = sizeof their_addr;
-	while (1) {
-		int numbytes;
-		char buf[PACKET_SIZE];
-		if ((numbytes = recvfrom(receive_socket, buf, PACKET_SIZE - 1, 0,
-				(struct sockaddr *) &their_addr, &addr_len)) == -1) {
-			perror("ack recv");
-			exit(1);
-		}
-		
-		//int ack_seq;
-
-		reliable_dgram *dgram = (reliable_dgram *)buf;
-		
-		//memcpy(&ack_seq, buf, INT_SIZE);
-		//ack_packet(ack_seq);
-		ack_packet(dgram->seq, dgram->window_size);
-	}
 }
